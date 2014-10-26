0c36e6133daf48ef75db9697dab866e611ad34e9
mmlxvi <quarterly24@gmail.com>
2014-10-26 15:26:36 -0500

    scrapped

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 //    $('#HTMLtext').toggle();
 //  })
 
-  var mainTextLibrary = ["images/todoText.png", "images/rrText.png", "images/artquizText.png", "images/fizzbuzzText.png",
-                          "images/hotorcoldText.png", "images/japanTVText.png"]
+//  var mainTextLibrary = ["images/todoText.png", "images/rrText.png", "images/artquizText.png", "images/fizzbuzzText.png",
+            //              "images/hotorcoldText.png", "images/japanTVText.png"]
 
 $('#site1').click(function(){
   $('#HTMLtext').toggle();
