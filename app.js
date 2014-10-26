3262d2a11f406096a04e8491014e8a1a83f615f0
mmlxvi <quarterly24@gmail.com>
2014-10-26 14:15:04 -0500

    add

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
 
   $('#envelope').click(function(){
     alert('working');
+    })
+
+ $('.siteFrames').click(function(){
+    $('#innerMain').toggle();
+    $('#innerMain2').toggle();
+
   })
 
+  var mainTextLibrary = ["images/todoText.png", "images/rrText.png", "images/artquizText.png", "images/fizzbuzzText.png",
+                          "images/hotorcoldText.png", "images/japanTVText.png"]
 
-
+$('#site1').click(function(){
+  $('#mainText2').append(mainTextLibrary[0]);
+})
 
 })
