c687db3bd5029705cb2735406e634047deaf4f64
mmlxvi <quarterly24@gmail.com>
2014-10-26 20:52:09 -0500

    final draft

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 $(document).ready(function() {
 
   $('#envelope').click(function(){
-    alert('working');
+    $('#contactImage').fadeToggle("slow");
+    $('#envelope').css("background-color", "white");
     })
 
- //$('.siteFrames').click(function(){
-//    $('#HTMLtext').toggle();
-//  })
 
-//  var mainTextLibrary = ["images/todoText.png", "images/rrText.png", "images/artquizText.png", "images/fizzbuzzText.png",
-            //              "images/hotorcoldText.png", "images/japanTVText.png"]
-
-$('#site1').click(function(){
-  $('#HTMLtext').toggle();
-  $('#textImage1').toggle();
-})
 
 })
